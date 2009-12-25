d395a5dcbbb8dd86ef2eeb6019400bc6c751ade3
daybreaker12 <daybreaker12@0b1a365c-e307-11de-b950-c1e2c078a2c5>
2009-12-25 08:04:29 +0000

    코드 정리

--- trunk/hama-mrcl/src/mrcl/mrcl/lib/MatrixMultiplier.java
+++ trunk/hama-mrcl/src/mrcl/mrcl/lib/MatrixMultiplier.java
@@ -3,6 +3,8 @@ package mrcl.lib;
 /**
  * Defines a matrix multiplier backend interface. 
  * This contains only one method that performs matrix multiplication.
+ * 
+ * All implementation class should be instantiatable with empty arguments.
  */
 public interface MatrixMultiplier
 {
